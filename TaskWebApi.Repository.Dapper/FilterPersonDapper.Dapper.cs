3c2acf9059861dfdc87cae500261a7c7819bb82a
giopalaga <girgi1201@gmail.com>
2021-05-18 05:49:15 +0400

    Refactored routes

---
+++ TaskWebApi.Repository.Dapper/FilterPersonDapper.Dapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+// ReSharper disable CollectionNeverQueried.Local
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace TaskWebApi.Repository.Dapper
+{
+    public class FilterPersonDapper : Connection
+    {
+        public static async Task<List<Person>> FilterPerson(string value)
+        {
+            await using var conn = new MySqlConnection(ConnStr);
+            await conn.OpenAsync();
+            const string sql = "SELECT * FROM persons_tbl WHERE Lname LIKE @a OR Fname LIKE @a OR Lname LIKE @a OR PrivateNumber LIKE @a OR PhoneNumber LIKE @a OR PersonId LIKE @a";
+            var persons = new List<Person>();
+            var z = await conn.QueryAsync<Person>(sql, new {@a = $"%{value}%"});
+            if (!z.Any())
+            {
+                return new List<Person>();
+            }
+            
+            return (List<Person>) z;
+        }
+    }
+}
--- WepApi/Controllers/PersonController.cs
+++ WepApi/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -21,6 +22,7 @@ namespace WepApi.Controllers
             _ = new GetPersonRep().GetPersonRepp(id);
             return (new GetPersonRep().GetPersonRepp(id) is null) ? NotFound("Id was not found in the database") : Ok(new GetPersonRep().GetPersonRepp(id));
         }
+        
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -28,7 +30,8 @@ namespace WepApi.Controllers
         {
             return DeletePersonRep.Delete(id).Result ? NotFound($"Person ${id} was not found") : Ok($"Person ${id} was deleted");
         }
-        [HttpPost]
+        
+        [HttpPost("insert/{person}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> InsertPerson([FromBody]Person person)
@@ -37,6 +40,15 @@ namespace WepApi.Controllers
             if (result.ErrorCode == ErrorList.OK) _ = new InsertPersonRep(person);
 
             return result.ErrorCode == ErrorList.OK ? Ok("User Inserted") : BadRequest(result.Description);
-        } 
+        }
+        
+        [HttpGet("search/{value}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Person>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Filter(string value)
+        {
+            var persons = FilterPerson.FilterRep(value).Result;
+            return persons.Any() ? Ok(persons) : NotFound();
+        }
     }
 }
