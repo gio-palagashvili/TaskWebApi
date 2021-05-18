94d28fb00f3d2962f683045515798b7d3eaf4246
giopalaga <girgi1201@gmail.com>
2021-05-19 02:49:13 +0400

    RelationType enum added and PersonRelations.cs was created

--- TaskWebApi.Common/PersonRelations.cs
+++ TaskWebApi.Common/PersonRelations.cs
@@ -1,11 +1,21 @@
 ﻿// ReSharper disable all UnusedMember.Local
 
+using System.Collections.Generic;
+
 namespace TaskWebApi
 {
     public class PersonRelations
     {
-        public string Index { get; set; }
+        public string Id { get; set; }
         public string PersonId { get; set; }
         public string RelationId { get; set; }
+        public List<RelationType> RelationType { get; set; }
+    }
+    public enum RelationType
+    {
+        Acquaintance = 0,
+        Colleague = 1,
+        Raltive = 2,
+        Other = 3
     }
 }
